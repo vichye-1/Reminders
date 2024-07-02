bdc12cf996758d7f512972be2e5d6597b29142d8
knockknock <yangban11111@gmail.com>
2024-07-03 02:15:07 +0900

    Realm Model 생성

--- Reminders/Database/ReminderModel.swift
+++ Reminders/Database/ReminderModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import RealmSwift
+
+class ReminderTable: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted(indexed: true) var reminderTitle: String
+    @Persisted var content: String?
+    @Persisted var dueDate: Date
+    @Persisted var tag: String?
+    @Persisted var priority: String?
+    
+    convenience init(reminderTitle: String, content: String? = nil, dueDate: Date, tag: String? = nil, priority: String? = nil) {
+        self.init()
+        self.reminderTitle = reminderTitle
+        self.content = content
+        self.dueDate = dueDate
+        self.tag = tag
+        self.priority = priority
+    }
+}
