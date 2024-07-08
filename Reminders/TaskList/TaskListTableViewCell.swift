5f596dd8da6cfb84754c1a3ceb72e7f76718a114
knockknock <yangban11111@gmail.com>
2024-07-08 19:24:23 +0900

    checkButton 위치 구현

--- Reminders/TaskList/TaskListTableViewCell.swift
+++ Reminders/TaskList/TaskListTableViewCell.swift
@@ -9,8 +9,31 @@ import UIKit
 import SnapKit
 
 class TaskListTableViewCell: BaseTableViewCell {
+    
+    private let checkButton = {
+        let button = UIButton()
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.systemGray.cgColor
+        button.layer.cornerRadius = 12
+        return button
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundColor = .customBackGround
+    }
+    
+    override func configureHierarchy() {
+        [checkButton].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    override func configureConstraints() {
+        checkButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(24)
+        }
     }
 }
